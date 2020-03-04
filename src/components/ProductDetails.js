abf07cc24edb53c5b7a406b46fb9381631155598
StudioME - Left iMac <Elliott@valtechcreative.com>
2020-03-04 13:47:35 -0500

    Starting Data Visualization

--- src/components/ProductDetails.js
+++ src/components/ProductDetails.js
@@ -4,12 +4,25 @@ import { graphql } from 'react-apollo';
 // Queries
 import { getImpressionsQuery } from '../queries/queries';
 
+// React Visualization
+import {
+    XYPlot,
+    XAxis,
+    YAxis,
+    ChartLabel,
+    HorizontalGridLines,
+    VerticalGridLines,
+    LineSeries,
+    LineSeriesCanvas
+  } from 'react-vis';
+
 class ProductDetails extends Component {
 
     state = {
         startDate: '',
         endDate: '',
         count: 0,
+        dateRange: [],
     }
 
     toNumber = (string) => {
@@ -23,6 +36,7 @@ class ProductDetails extends Component {
         const startDate = this.toNumber(this.state.startDate);
         const endDate = this.toNumber(this.state.endDate);
         let count = 0;
+        let dateRange = [{x: startDate, y: 1}];
 
         for (let i = 0; i < impressions.length; i++) {
             if (startDate <= this.toNumber(impressions[i].date) && this.toNumber(impressions[i].date) <= endDate) {
@@ -30,14 +44,29 @@ class ProductDetails extends Component {
             }
         }
 
+        let tempDate = startDate;
+        let xAxis = 1 ;
+        for (let i = startDate; i <  endDate; i++) {
+            tempDate = tempDate + 1;
+            xAxis = xAxis + 1;
+            dateRange.push({x: tempDate, y: xAxis});
+        }
+
         this.setState({
             ...this.state,
             count: count,
+            dateRange, dateRange
         })
     }
 
     displayImpressions = () => {
         const { product } = this.props.data;
+        const data = [
+            {x: 1-2-20, y: 1},
+            {x: 1-3-20, y: 4},
+            {x: 1-4-20, y: 2},
+            {x: 1-5-20, y: 1},
+        ]
 
         if (product) {
             return(
@@ -63,6 +92,37 @@ class ProductDetails extends Component {
                     <button className="details__button" onClick={this.filterDates}>Submit</button>
 
                     <p className="details__range-total">{this.state.count}</p>
+
+                    <XYPlot width={400} height={300}>
+                        <HorizontalGridLines />
+                        <VerticalGridLines />
+                        <YAxis />
+                        <ChartLabel 
+                            text="Date"
+                            className="alt-x-label"
+                            includeMargin={false}
+                            xPercent={0.025}
+                            yPercent={1.01}
+                            />
+
+                        <ChartLabel 
+                            text="Impressions"
+                            className="alt-y-label"
+                            includeMargin={false}
+                            xPercent={0.06}
+                            yPercent={0.06}
+                            style={{
+                            transform: 'rotate(-90)',
+                            textAnchor: 'end'
+                            }}
+                            />
+                        <LineSeries
+                            stroke={'blue'}
+                            strokeWidth={'5px'}
+                            opacity={1}
+                            data={data}
+                        />
+                    </XYPlot>
                 </React.Fragment>
             )
         } else {
